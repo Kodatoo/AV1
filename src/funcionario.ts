01a3889ba789a3d094422a3f92b557d8336e52a1
Kodatoo <gkodatofaria@gmail.com>
2025-10-04 01:48:10 -0300

    feat: metodo salvar na classe Funcionarios

--- src/funcionario.ts
+++ src/funcionario.ts
@@ -1,3 +1,7 @@
+import * as path from 'path';
+import * as fs from 'fs';
+
+
 export default class Funcionario {
     public id: string
     public nome: string
@@ -34,9 +38,44 @@ export default class Funcionario {
 
     get getNivel(): string { return this.nivel }
 
-}   
+    public salvar = (): void => {
+        const FuncionarioData = {
+            id: this.getId,
+            nome: this.nome,
+            telefone: this.telefone,
+            endereco: this.endereco,
+            usuario: this.usuario,
+            senha: this.senha,
+            nivel: this.nivel
+        }
 
+        const publicDirPath = path.join(__dirname, '..', 'public')
+        const filePath = path.join(__dirname, '..', 'public', 'funcionarios.json')
 
+        if (!fs.existsSync(publicDirPath)) {
+            fs.mkdirSync(publicDirPath, { recursive: true })
+        }
+
+         try {
+            let funcionarios = []
+
+            if (fs.existsSync(filePath)) {
+                const data = fs.readFileSync(filePath, 'utf-8')
+                funcionarios = JSON.parse(data) // converte json em array de objetos
+            }
+            
+            funcionarios.push(FuncionarioData)
+
+             fs.writeFileSync(filePath, JSON.stringify(funcionarios, null, 2), 'utf-8')
+             console.log("Funcionarios salvo com sucesso!")
+
+         }
+
+         catch(err){
+            console.log(`Erro ao salvar funcionarios: ${err}`)
+         }
+
+        }}
 
 
 
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,25 @@
 import Aeronave, {TipoAeronave} from "./aeronaves";
+import Funcionario, { NivelPermissao } from "./funcionario";
 import Tipo, { TipoPeca, StatusPeca } from './peca';
 
 let teste = new  Aeronave('231313', 'f22', TipoAeronave.COMERCIAL, 22, 30)
 
 const minhaPeca = new Tipo('Motor X1', TipoPeca.NACIONAL, 'Fornecedor A', StatusPeca.EM_PRODUCAO);
 
-minhaPeca.atualizarStatus();
+let funcionariosteste = new Funcionario("1", "Koda", "1293810302130", "Rua dos carai", "UsuarioMASTER","ratos", NivelPermissao.ENGENHEIRO )
+
+
 
 teste.salvar();
 
 console.log('Status inicial:', minhaPeca.getStatus);
 
-console.log(teste.detalhes())
+
+console.log(teste.detalhes());
+
+funcionariosteste.salvar();
+
+console.log(funcionariosteste);
+
+minhaPeca.atualizarStatus();
 
