fbcfeaa781fa66f386dd84172ed932274a29ce49
Kodatoo <gkodatofaria@gmail.com>
2025-10-04 00:34:54 -0300

    feat: adição do método AtualizaStatus

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import Aeronave, {TipoAeronave} from "./aeronaves";
+import Tipo, { TipoPeca, StatusPeca } from './peca';
 
 let teste = new  Aeronave('231313', 'f22', TipoAeronave.COMERCIAL, 22, 30)
+
+const minhaPeca = new Tipo('Motor X1', TipoPeca.NACIONAL, 'Fornecedor A', StatusPeca.EM_PRODUCAO);
+
+minhaPeca.atualizarStatus();
+
+console.log('Status inicial:', minhaPeca.getStatus);
 
 console.log(teste.detalhes())
--- src/peca.ts
+++ src/peca.ts
@@ -1,34 +1,58 @@
-export default class Tipo{
-    public nome: string
-    public tipo: TipoPeca
-    public fornecedor: string
-    public status: StatusPeca
+import * as readline from "readline";
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+export default class Tipo {
+    public nome: string;
+    public tipo: TipoPeca;
+    public fornecedor: string;
+    public status: StatusPeca;
     
-    constructor(nome: string, tipo: TipoPeca, fornecedor: string, status: StatusPeca){
-        this.nome = nome
-        this.tipo = tipo
-        this.fornecedor = fornecedor
-        this.status = status
+    constructor(nome: string, tipo: TipoPeca, fornecedor: string, status: StatusPeca) {
+        this.nome = nome;
+        this.tipo = tipo;
+        this.fornecedor = fornecedor;
+        this.status = status;
     }
 
-    get getNome(): string { return this.nome  }
+    get getNome(): string { return this.nome; }
+    get getTipo(): TipoPeca { return this.tipo; }
+    get getFornecedor(): string { return this.fornecedor; }
+    get getStatus(): StatusPeca { return this.status; }
 
-    get getTipo(): string { return this.tipo }
+    public atualizarStatus(): void {
+        const statusList = Object.values(StatusPeca);
 
-    get getFornecedor(): string { return this.fornecedor }
+        console.log('\nStatus disponíveis:');
+        statusList.forEach((status, index) => {
+            console.log(`${index} - ${status}`);
+        });
 
-    get getStatus(): string { return this.status }
+        rl.question('\nDigite o número correspondente ao novo status: ', (resposta) => {
+            const index = parseInt(resposta); //parse converte a resposta que é string em inteiro
 
+            if (isNaN(index) || index < 0 || index >= statusList.length) { // isnan verificar se o valor não é numero
+                console.log(' Opção Inválida!');
+            } else {
+                this.status = statusList[index] as StatusPeca;
+                console.log(`Status atualizado para: ${this.status}`);
+            }
+
+            rl.close(); // termina o input
+        });
+    }
 }
 
-
-export enum StatusPeca{
+export enum StatusPeca {
     EM_PRODUCAO = 'EM_PRODUCAO',
     EM_TRANSPORTE = 'EM_TRANSPORTE',
     PRONTA = 'PRONTA'
 }
 
-export enum TipoPeca{
+export enum TipoPeca {
     NACIONAL = 'NACIONAL',
     IMPORTADA = 'IMPORTADA'
 }
